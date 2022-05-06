d7b05b6b5d257a254815c1f41408d588c7d9858b
j. jillian <judith.mediosdigitales@gmail.com>
2022-05-06 21:13:59 +0200

    solve lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,51 @@
 // Soldier
-class Soldier {}
-console.log('test');
+class Soldier {
+  constructor(health, strength){
+    this.health = health
+    this.strength = strength
+  }
+  attack () {
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health = this.health - damage;
+  }
+}
+
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(health, strength)
+    this.name = name
+    
+  }
+  battleCry(){
+    return  'Odin Owns You All!'
+  }
+}
+
+// const Jillian = new Soldier (100, 10);
+// Jillian.receiveDamage(30);
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength){
+    super(health, strength)
+  }
+  receiveDamage(damage){
+    this.health = this.health - damage;
+    if(this.health > 0){
+      return `A ${this.name} has received ${damage} points of damage`
+    } else {
+      return `A ${this.name} has died in combat` 
+    }
+      
+    }
+  }
+    
+
+//  
+// }
 
 // War
 class War {}
